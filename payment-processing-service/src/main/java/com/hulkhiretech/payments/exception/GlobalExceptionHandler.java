6f2f8bef9e9c635f28848aedaea14554a167dfe3
Somshekhar Arabali <codelord07@gmail.com>
2025-11-30 00:18:03 +0530

    Add custom exception handling

---
+++ payment-processing-service/src/main/java/com/hulkhiretech/payments/exception/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.hulkhiretech.payments.exception;
+
+import com.hulkhiretech.payments.constant.ErrorCodeEnum;
+import com.hulkhiretech.payments.pojo.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(ProcessingException.class)
+    public ResponseEntity<ErrorResponse> handleProcessingException(
+            ProcessingException ex) {
+        log.error("Handling handleProcessingException: {}", ex.getMessage(), ex);
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getErrorCode(),
+                ex.getMessage()
+        );
+
+        log.info("ErrorResponse: {}", errorResponse);
+        return new ResponseEntity<>(errorResponse, ex.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(
+            Exception ex) {
+        log.error("Handling handleException: {}", ex.getMessage(), ex);
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                ErrorCodeEnum.GENERIC_ERROR.getErrorCode(),
+                ErrorCodeEnum.GENERIC_ERROR.getErrorMessage()
+        );
+
+        log.info("ErrorResponse: {}", errorResponse);
+        return new ResponseEntity<>(
+                errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
